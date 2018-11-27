0739c5a91345b3cdf168ecc550afce2d998d64b7
s-sandra <aleksandra.shtabnaya@gmail.com>
2018-11-26 20:44:34 -0500

    Added PHP functionality that only displays log-in link in navigation if user not logged in.

--- header.php
+++ header.php
@@ -8,11 +8,17 @@
         <!--<meta name="description" content=".">-->
         <!--<link rel="stylesheet" href="./styles.css">-->
     </head>
+    <?php session_start(); ?>
 
     <body>
     <ul>
         <li><a href="search.php">search</a></li>
-        <li><a href="login.php">log in</a></li>
-        <li><a href="account.php">account</a></li>
-        <li><a href="logout.php">log out</a></li>
+        <?php 
+        if($_SESSION["LoggedIn"]){
+            echo "<li><a href='account.php'>account</a></li>
+            <li><a href='logout.php'>log out</a></li>";
+        }
+        else {
+            echo "<li><a href='login.php'>log in</a></li>";
+        }?>
     </ul>
--- login.php
+++ login.php
@@ -2,5 +2,8 @@
     $page_title = "Log In";
     include("header.php");
 ?>
+
+<!---I called the session boolean variable to state if a user is logged in or not as "LoggedIn". 
+You will set this equal to true if the user successfully logged in.--->
     </body>
 </html>
