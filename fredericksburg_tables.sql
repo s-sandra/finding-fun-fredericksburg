a3b23d1e05b0bda9c37187dbbdcc8f6beb717205
s-sandra <aleksandra.shtabnaya@gmail.com>
2018-11-28 14:43:29 -0500

    Added location table.

---
+++ fredericksburg_tables.sql
@@ -0,0 +1,6 @@
+CREATE TABLE location (
+    location_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    street_address VARCHAR(100) NOT NULL,
+    zip_code VARCHAR(5) NOT NULL
+);
