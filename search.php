c497e07d0a0c193388da18dfa58f37ae33a9b7b5
s-sandra <aleksandra.shtabnaya@gmail.com>
2018-11-28 15:41:46 -0500

    Began php for search results.

--- search.php
+++ search.php
@@ -3,13 +3,19 @@
     include("header.php");
 ?>
 
-<form method="post" action="search-results.php" id="searchform">
-    <input type="checkbox" name="food" value="food">Food
-    <input type="checkbox" name="entertainment" value="entertainment">Entertainment
-    <input type="checkbox" name="recreation" value="recreation">Recreation<br>
+<form method="post" action="search.php?results" id="searchform">
+    <input type="checkbox" name="category_list[]" value="food">Food
+    <input type="checkbox" name="category_list[]" value="entertainment">Entertainment
+    <input type="checkbox" name="category_list[]" value="recreation">Recreation<br>
     <input type="text" name="query">
     <input type="submit" name="submit" value="search">
 </form>
+
+<?php
+    if(isset($_GET['results'])){
+        $category = "nothing";
+    }
+?>
 
 <?php
     include("footer.php");
