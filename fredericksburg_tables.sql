55421483f138d55bb1d95d6d21e2268d72dcb966
s-sandra <aleksandra.shtabnaya@gmail.com>
2018-11-28 14:51:51 -0500

    Added location_category junction.

--- fredericksburg_tables.sql
+++ fredericksburg_tables.sql
@@ -9,3 +9,15 @@ CREATE TABLE category (
     category_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(10)
 );
+
+CREATE TABLE location_category (
+    location_id INT NOT NULL,
+    category_id INT NOT NULL,
+    PRIMARY KEY (location_id, category_id),
+    CONSTRAINT location_id_fk
+    FOREIGN KEY (location_id)
+    REFERENCES location (location_id),
+    CONSTRAINT category_id_fk
+    FOREIGN KEY (category_id)
+    REFERENCES category (category_id)
+);
