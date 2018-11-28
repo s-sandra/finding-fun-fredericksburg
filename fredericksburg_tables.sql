9c28693ebf96dc9af5ceef34c50afef4a65cf5c8
s-sandra <aleksandra.shtabnaya@gmail.com>
2018-11-28 14:45:45 -0500

    Added category table.

--- fredericksburg_tables.sql
+++ fredericksburg_tables.sql
@@ -4,3 +4,8 @@ CREATE TABLE location (
     street_address VARCHAR(100) NOT NULL,
     zip_code VARCHAR(5) NOT NULL
 );
+
+CREATE TABLE category (
+    category_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(10)
+);
