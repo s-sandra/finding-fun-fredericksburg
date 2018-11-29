72ebaf19619a836f9afc76e55da6cc6a58b7b454
s-sandra <aleksandra.shtabnaya@gmail.com>
2018-11-29 11:11:18 -0500

    Added template file for database connection.

---
+++ connection.php
@@ -0,0 +1,6 @@
+<?php
+    include "credentials.php";
+    $servername = "localhost";
+    $databaseName = "TEAM_350_Fall18_Gold";
+    $connection = mysqli_connect($servername, $username, $password, $databaseName);
+?>
