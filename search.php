24c81c3e13f7897d3006fcbd1209a6a353bc2790
s-sandra <aleksandra.shtabnaya@gmail.com>
2018-11-29 10:26:45 -0500

    Wrote sql conditional for search by category.

--- search.php
+++ search.php
@@ -4,16 +4,26 @@
 ?>
 
 <form method="post" action="search.php?results" id="searchform">
-    <input type="checkbox" name="category_list[]" value="food">Food
-    <input type="checkbox" name="category_list[]" value="entertainment">Entertainment
-    <input type="checkbox" name="category_list[]" value="recreation">Recreation<br>
+    <input type="checkbox" name="category_list[]" value="food"><label>Food</label>
+    <input type="checkbox" name="category_list[]" value="entertainment"><label>Entertainment</label>
+    <input type="checkbox" name="category_list[]" value="recreation"><label>Recreation</label><br>
     <input type="text" name="query">
     <input type="submit" name="submit" value="search">
 </form>
 
 <?php
     if(isset($_GET['results'])){
-        $category = "nothing";
+        if (!empty($_POST['category_list'])){
+            $categories = "WHERE category IN [";
+            $lastElement = end($_POST['category_list'] );
+            foreach($_POST['category_list'] as $category){
+                $categories = $categories . " '" . $category . "'";
+                if($category != $lastElement){
+                    $categories = $categories . ", ";
+                }
+            }
+            $categories = $categories . "];";
+        }
     }
 ?>
 
