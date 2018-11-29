aa301cfb388a17df1b694c7bceefa389df194392
s-sandra <aleksandra.shtabnaya@gmail.com>
2018-11-29 17:35:19 -0500

    Added conditional for search phrase.

--- search.php
+++ search.php
@@ -12,17 +12,40 @@
 </form>
 
 <?php
+    // checks to see if user hit submit and passed results as get super global.
     if(isset($_GET['results'])){
-        if (!empty($_POST['category_list'])){
-            $categories = "WHERE category IN [";
-            $lastElement = end($_POST['category_list'] );
-            foreach($_POST['category_list'] as $category){
-                $categories = $categories . " '" . $category . "'";
-                if($category != $lastElement){
-                    $categories = $categories . ", ";
+
+        // checks to see if user entered search phrase.
+        $search = $_POST['query'];
+        if(!empty($search)){
+            $categories = "";
+    
+            // checks to see if user clicked on a category
+            if (!empty($_POST['category_list'])){
+                $categories = "category IN [";
+                $lastElement = end($_POST['category_list'] );
+                foreach($_POST['category_list'] as $category){
+                    $categories = $categories . " '" . $category . "'";
+                    if($category != $lastElement){
+                        $categories = $categories . ", ";
+                    }
                 }
+                $categories = $categories . "];";
             }
-            $categories = $categories . "];";
+    
+            // connection.php connects to database.
+            include("connection.php");
+            $sql_select = "SELECT loc.name, loc.street_address, loc.zip_code, cat.category FROM location AS loc 
+                            NATURAL JOIN location_category AS loc_cat INNER JOIN category AS cat
+                            ON loc_cat.category_id = cat.category_id " . $categories;
+            
+            $sql_select = $sql_select . "WHERE loc.name LIKE '" . $search . "'";
+            if (!empty($categories)){
+                $sql_select = $sql_select . " AND " . $categories;
+            }
+        }
+        else{
+            echo "<p>Please enter a search phrase.</p>";
         }
     }
 ?>
