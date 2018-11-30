3438441d1a79584790c31357beb8c7bb8b373155
s-sandra <aleksandra.shtabnaya@gmail.com>
2018-11-30 17:53:03 -0500

    Connected to database, printed out locations matching search query.

--- search.php
+++ search.php
@@ -35,16 +35,35 @@
     
             // connection.php connects to database.
             include("connection.php");
-            $sql_select = "SELECT loc.name, loc.street_address, loc.zip_code, cat.category FROM location AS loc 
+            $sql_select = "SELECT loc.name AS name, loc.street_address AS address, loc.zip_code AS zip FROM location AS loc 
                             NATURAL JOIN location_category AS loc_cat INNER JOIN category AS cat
                             ON loc_cat.category_id = cat.category_id " . $categories;
             
-            $sql_select = $sql_select . "WHERE loc.name LIKE '" . $search . "'";
+            $sql_select = $sql_select . "WHERE loc.name LIKE ?";
             if (!empty($categories)){
                 $sql_select = $sql_select . " AND " . $categories;
             }
+
+            if ($prepared = mysqli_prepare($connection, $sql_select)){
+                
+                // s stands for string. Replaces ? in select statement with search variable.
+				mysqli_stmt_bind_param($prepared, "s", $search);
+				
+				mysqli_stmt_execute($prepared);
+				$result = mysqli_stmt_get_result($prepared);
+                
+                echo "<h3>Search results for '" . $search . "'</h3>\n";
+                echo "<ul>";
+
+				// Prints out search results as unordered list items.
+				while ($row = mysqli_fetch_assoc($result)) {
+					echo "<li>" . $row["name"] . " | Average rating: <br>" . $row['address'] . " Fredericksburg, VA, " . $row['zip'] . "</li>\n";
+                }
+                
+                echo "</ul>";
+            }
         }
-        else{
+        else {
             echo "<p>Please enter a search phrase.</p>";
         }
     }
