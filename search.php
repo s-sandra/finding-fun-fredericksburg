174ea0f5c000f1103d60538726aba3bf53b2185f
s-sandra <aleksandra.shtabnaya@gmail.com>
2018-12-01 19:27:24 -0500

    Added and tested search by phrase functionality.

--- search.php
+++ search.php
@@ -42,7 +42,7 @@
             if (!empty($search)){
                 $sql_select = "SELECT loc.name AS name, loc.street_address AS address, loc.zip_code AS zip FROM location AS loc 
                                 NATURAL JOIN location_category AS loc_cat INNER JOIN category AS cat
-                                ON loc_cat.category_id = cat.category_id WHERE loc.name LIKE '%?%'";
+                                ON loc_cat.category_id = cat.category_id WHERE loc.name LIKE ?";
                 
                 if (!empty($categories)){
                     $sql_select = $sql_select . " AND " . $categories;
@@ -61,30 +61,29 @@
             // connection.php connects to database.
             include("connection.php");
 
-            if ($prepared = mysqli_prepare($connection, $sql_select)){
-                
-                // s stands for string. Replaces ? in select statement with search variable.
-				mysqli_stmt_bind_param($prepared, "s", $sql_select);
-				
-				mysqli_stmt_execute($prepared);
-				$result = mysqli_stmt_get_result($prepared);
-                
-                if(!empty($search)){
+            if(!empty($search)){
+                if ($prepared = mysqli_prepare($connection, $sql_select)){
+                    // s stands for string. Replaces ? in select statement with search variable.
                     echo "<h3>Search results for '" . $search . "'</h3>\n";
+                    $search = "%" . $search . "%";
+                    mysqli_stmt_bind_param($prepared, "s", $search);
+                    mysqli_stmt_execute($prepared);
+				    $result = mysqli_stmt_get_result($prepared);
                 }
-                else{
-                    echo "<h3>Search results for '" . $category_list . "'</h3>\n";
-                }
-                
-                echo "<ul>";
-
-				// Prints out search results as unordered list items.
-				while ($row = mysqli_fetch_assoc($result)) {
-					echo "<li>" . $row["name"] . " | Average rating: <br>" . $row['address'] . " Fredericksburg, VA, " . $row['zip'] . "</li>\n";
-                }
-                
-                echo "</ul>";
             }
+            else{
+                $result = mysqli_query($connection, $sql_select);
+                echo "<h3>Search results for '" . $category_list . "'</h3>\n";
+            }
+                
+            echo "<ul>";
+
+			// Prints out search results as unordered list items.
+			while ($row = mysqli_fetch_assoc($result)) {
+				echo "<li>" . $row["name"] . " | Average rating: <br>" . $row['address'] . " Fredericksburg, VA, " . $row['zip'] . "</li>\n";
+            }
+                
+            echo "</ul>";
         }
         else {
             echo "<p>Please enter a search phrase or choose a category.</p>";
