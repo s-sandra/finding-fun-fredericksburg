6d8972afc6fedde37ac13890bfd0c83072de7e7f
Jessica Thomas <jessthomas1105@gmail.com>
2018-12-04 21:43:30 -0500

    Update register.php
    Table set-up for registration page

--- register.php
+++ register.php
@@ -1,10 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+</head>
+
+<body bgcolor=lightblue">
+
 <?php
-  $page_title = "Register";
-  include("header.php");
-  ?>
+    $page_title = "Log In";
+    include("header.php");
+?>
+
+<form action="register.php" method"post">
+  <table border="2" align="center" bgcolor="white">
+<tr>
+<td colspan="2" alight="center">REGISTER</td>
+</tr>
+
+<tr>
+<td>Full Name:</td>
+<td><input type = "text" name = "name"></td>
+</tr>
+
+<tr>
+<td>Username:</td>
+<td><input type = "text" name = "username"></td>
+</tr>
+
+<tr>
+<td>Password:</td>
+<td><input type = "text" name = "password"></td>
+</tr>
   
+<tr>
+<td>Security Question:</td>
+<td><input type = "text" name = "secquestion"></td>
+</tr>
   
-  
+<tr>
+<td>Answer:</td>
+<td><input type = "text" name = "answer"></td>
+</tr>
+
+<td colspan="2" align="center"><input type="submit" value="Submit"></td>
+</tr>
+</table>
+</form>
+
+
+<?php
+include "credentials.php";
+include "connection.php";
+?>
+
 <?php
 include("footer.php");
 ?>
