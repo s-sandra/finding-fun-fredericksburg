b855c65259a164043c73bb10e3d66d733852ae8e
Sabine Wills <swills@mail.umw.edu>
2018-12-09 15:18:26 -0500

    added Recreation part of home page

--- index.php
+++ index.php
@@ -3,6 +3,20 @@
     $page_title = "Finding Fun in Fredericksburg";
     include("header.php");
 ?>
+
+<body>
+    <h1> Recreation </h1>
+    <p>
+        "The City of Fredericksburg has plenty of fun things to do! There are business, parks,
+        and trails that are all visited by locals and visitors alike.
+        
+        <a href="http://cs.umw.edu/~swills/finding-fun-fredericksburg/search.php"> Click here to search Recreation" </a>
+
+    </p>
+
+
+</body>
 <?php
     include("footer.php");
 ?>
+
