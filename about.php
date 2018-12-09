c8e17058b1674ef6fa34516cb66188c7be5ece52
Sabine Wills <swills@mail.umw.edu>
2018-12-09 16:06:10 -0500

    Added text to about page

--- about.php
+++ about.php
@@ -2,3 +2,14 @@
     $page_title = "About";
     include("header.php");
 ?>
+
+<body>
+    <h1> About </h1>
+    <p>
+        We wanted to create a website that would help visitors and locals of the Fredericksburg
+        area find places to support local businesses and enjoy themselves. We have done so by
+        including locations and categorizing them in a way that is easily searchable. You are 
+        welcome to submit reviews of the locations that we post, and we hope you enjoy!
+    </p>
+    </h1>
+</body>
