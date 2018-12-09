a80847cda06bb39c73423621659c48801cb73eac
Sabine Wills <swills@mail.umw.edu>
2018-12-09 15:50:59 -0500

    Finished homepage

--- index.php
+++ index.php
@@ -5,12 +5,30 @@
 ?>
 
 <body>
-    <h1> Recreation </h1>
+    <h1> Entertainment </h1>
     <p>
         The City of Fredericksburg has plenty of fun things to do! There are business, parks,
         and trails that are all visited by locals and visitors alike.
         
-        <a href="http://cs.umw.edu/~swills/finding-fun-fredericksburg/search.php"> Click here to search Recreation </a>
+        <a href="http://cs.umw.edu/~swills/finding-fun-fredericksburg/search.php"> Click here to search Entertainment </a>
+
+    </p>
+
+    <h1> Food </h1>
+    <p>
+        There are so many wonderful places to eat with different tastes and cultures in this fine 
+        city that you can experience through food.
+        
+        <a href="http://cs.umw.edu/~swills/finding-fun-fredericksburg/search.php"> Click here to search food </a>
+
+    </p>
+
+    <h1> Shopping </h1>
+    <p>
+        Fredericksburg has a variety of different shops and shopping opportunities like the Farmer's market 
+        that we can inform you about through reviews and descriptions.
+        
+        <a href="http://cs.umw.edu/~swills/finding-fun-fredericksburg/search.php"> Click here to search Shopping </a>
 
     </p>
 
@@ -19,7 +37,3 @@
 <?php
     include("footer.php");
 ?>
-<<<<<<< HEAD
-=======
-
->>>>>>> b855c65259a164043c73bb10e3d66d733852ae8e
